a507a8c1b7947a680981d24f40915898b1c3e0f1
Emma <emma.phommatha@gmail.com>
2018-04-07 00:01:07 -0600

    update-css

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,14 +5,20 @@ function Product(props) {
   const { name, price, imageurl, productid } = props;
   return (
 
-    <div className="display">
-      <div className="each-box">
-        <img src={imageurl} alt='pic' />
-        <h2>{name}</h2>
-        <p>${price}</p>
+    <div className="redbox">
+      <img className="img" src={imageurl} alt='pic' />
+      <div className="content">
+        <div className='namePriceContainer'>
+          <p>{name}</p>
+          <p>${price}</p>
+        </div>
+
+        <div className='buttonsContainer'>
+          <button>Delete</button>
+          <button>Edit</button>
+        </div>
       </div>
     </div>
-
   )
 }
 
